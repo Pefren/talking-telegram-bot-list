877512e372c922723b7b03a6d2e5c0d2f9cb8b94
pefren <pefren@farma.dev>
2018-07-30 11:02:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let list = {
+    'default':[
+        'Say something about yourself.',
+        'How are you?'
+    ],
+    'hi|hello':[
+        'Hello!',
+        'Hi! Nice to meet you.',
+        'Hi.'
+    ],
+    'how are you|what\'s up':[
+        'Not bad.',
+        'Nice. How are you?',
+        'Everything is fine.',
+        'Good. And you?'
+    ],
+    'nice|good|excellent|not bad':[
+        'Good',
+        'Okay',
+        'Nice'
+    ]
+};
+
+module.exports = list;
